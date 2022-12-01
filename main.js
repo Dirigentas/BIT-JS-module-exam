b6bcbba9ce047496665b06c75cb3eae1b4e6085f
Aras <114133924+Dirigentas@users.noreply.github.com>
2022-12-01 14:14:38 +0200

    initial

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('labas');
+document.querySelector('div').innerHTML = 'labas rytas';
+document.getElementById('main').innerHTML += ' viso gero';
+// DOM.classList.add('virykle');
+
+// level2DOM.addEventListener('mouseenter', () => {
+//     level2DOM.innerHTML = `<div class = "level2box">labas</div>`
+// })
